7caaa322f5a2521f0bc11d94bd6f98f535b48225
maskaywalanay <mwalanay@gmail.com>
2019-10-20 10:32:44 +0700

    komit awal

---
+++ SwitchPreferenceSimple/app/src/main/java/ngopi/siang/vrohh/MainActivity.java
@@ -0,0 +1,14 @@
+package ngopi.siang.vrohh;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
